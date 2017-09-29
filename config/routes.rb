e8b588a1b41949aa90685f113235d32668f245da
Antoine AYOUB <antoine.ayoub@gmail.com>
2017-09-29 17:33:42 +0200

    change devise registrations route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => { registrations: 'registrations' }
 
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
